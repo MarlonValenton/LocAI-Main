d45e8b98c7588de2d5de63f9881e7e37608341b3
Cyrus <cyrusabraham.olega.cics@ust.edu.ph>
2024-11-13 21:29:13 +0800

    fix: hide system message appearing in DeleteDialog component
    - fix DeleteDialog extra gap between header, content, and footer

--- src/App/components/Dialogs/DeleteDialog.tsx
+++ src/App/components/Dialogs/DeleteDialog.tsx
@@ -15,17 +15,21 @@ export function DeleteDialog({chatHistory, children, onDelete}: DeleteDialogProp
         <div onClick={(e) => e.stopPropagation()}>
             <Dialog>
                 {children}
-                <DialogContent className="max-w-[50%] p-0 m-0">
+                <DialogContent className="max-w-[50%] p-0 m-0 gap-0">
                     <DialogHeader className="px-[15px] pt-[15px] pb-[10px] border-border-gray border-b-[1px] h-fit">
                         <DialogTitle>Delete Item</DialogTitle>
                         <DialogDescription>Are you sure you want to delete this item?</DialogDescription>
                     </DialogHeader>
                     <div className="h-[calc(100vh/2)] overflow-auto">
-                        {chatHistory.map((item, index) => (
-                            <ChatSingle index={index} type={item.type}>
-                                {item.type === "model" ? item.response[0]!.toString() : item.text.toString()}
-                            </ChatSingle>
-                        ))}
+                        {chatHistory.map((item, index) => {
+                            if (item.type !== "system") {
+                                return (
+                                    <ChatSingle index={index} type={item.type}>
+                                        {item.type === "model" ? item.response[0]!.toString() : item.text.toString()}
+                                    </ChatSingle>
+                                );
+                            } else return <></>;
+                        })}
                     </div>
                     <DialogFooter className="border-border-gray border-t-[1px] px-[15px] py-[10px]">
                         <DialogClose asChild>
